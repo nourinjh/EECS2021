53ab7f8d26216de850c542347e8c9fa1a67fd280
Nourin Abd El Hadi <nourin@ea24.eecs.yorku.ca>
2019-09-30 20:15:26 -0400

    made some changes

--- PreLab3/cex2b.asm
+++ PreLab3/cex2b.asm
@@ -1,29 +1,25 @@
-s0:	DC	"Enter n:\0"
-s1:	DC	"ans:\0"
-a:	DF	1.0
-	
-	addi x5, x0, s0;text stuff
-	ecall x1, x5, 4
-	ecall x6, x0, 5
-	add x7, x6, x0;temp variable
-	addi x7, x7, -1;decreciating the var
-	add x12, x6, x0
-	addi x12, x12, -1
-	fld f1, a(x0)
-	
-loop:   mul x8, x7, x6
-	add x6, x0, x8
-	addi x7, x7, -1
-	bne x7, x0, loop
-	addi x5, x0, s1
-	ecall x1, x5, 4
-	ecall x2, x6, 0
+d: 	DF 	1.0
 
-loops:	fcvt.d.l f2, x6
-	fdiv.d f5, f1, f2
-	fadd.d f6, f6, f5
-	addi x12, x12, -1
-	bne x12, x0, loops
-	;fcvt.l.d x20, f6
-	;ecall x2, x20, 0
-	ebreak x0, x0, 0  
+	ecall x6, x0, 5 ;input
+	fld f2, d(x0)
+	fadd.d f10, f10, f2
+	addi x7, x0, 1
+
+big:	beq x6, x0, end
+	add x5, x0, x6
+	addi x7, x0, 1
+	beq x0, x0, loop
+
+loop:	beq x5, x0, decr
+	mul x7, x7, x5
+	addi x5, x5, -1
+	beq x0, x0, loop
+
+decr: 	addi x6, x6, -1
+	fcvt.d.l f1, x7
+	fdiv.d f9, f2, f1
+	fadd.d f10, f10, f9
+	beq x0, x0, big
+
+end:	ebreak x0,x0,0
+
